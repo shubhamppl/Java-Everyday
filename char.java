37a741344eb3217fe435da4147c2515540f68479
shubhamppl <64500580+shubhamppl@users.noreply.github.com>
2021-11-14 15:12:49 +0530

    it is aexample of switch statement

---
+++ char.java
@@ -0,0 +1,21 @@
+public class Main
+{
+	public static void main(String[] args) {
+	char letter= 'o';
+	switch(letter)
+	{
+	    case 'a':
+	    case 'o':
+	        System.out.println("this is an vowel");
+	        case 'b':
+	            case 'c':
+	                System.out.println("this is alphabet");
+	                break;
+	                case '*':
+	                case '#':
+	                System.out.println("special character");
+	                break;
+	}
+	}	}
+	
+
